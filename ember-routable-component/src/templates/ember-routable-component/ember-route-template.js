e5bd944ac28aff2b5e0960f16de5a25a572e5bdd
Patrick Pircher <patricklx@users.noreply.github.com>
2024-03-18 14:39:44 +0100

    fix controller arg

--- ember-routable-component/src/templates/ember-routable-component/ember-route-template.js
+++ ember-routable-component/src/templates/ember-routable-component/ember-route-template.js
@@ -1,2 +1,2 @@
 import { precompileTemplate } from '@ember/template-compilation';
-export default precompileTemplate("<this.Component @model={{this.model}} @controller={{this.controller}} />");
+export default precompileTemplate("<this.Component @model={{this.model}} @controller={{this}} />");
